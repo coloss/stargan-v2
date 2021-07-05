761a4ffe19a28ba95ba3f3917ba3f6bc5f17eec3
Radek Danecek <danekradek@gmail.com>
2021-07-05 09:42:14 +0200

    Fix evaluation step crash for deterministic StarGAN

--- core/solver.py
+++ core/solver.py
@@ -187,7 +187,8 @@ class Solver(SolverBase):
         nets_ema = self.nets_ema
         resume_iter = args.resume_iter
         self._load_checkpoint(args.resume_iter)
-        calculate_metrics(nets_ema, args, step=resume_iter, mode='latent')
+        if self.args.latent_dim > 0:
+            calculate_metrics(nets_ema, args, step=resume_iter, mode='latent')
         calculate_metrics(nets_ema, args, step=resume_iter, mode='reference')
 
 
